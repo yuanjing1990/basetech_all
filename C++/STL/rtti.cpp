00aa460ce4116ba1089a05ff24d2a29408a19518
YuanJing <695447272@qq.com>
2018-01-25 18:12:14 +0800

    忽略exe文件

--- C++/STL/rtti.cpp
+++ C++/STL/rtti.cpp
@@ -21,26 +21,26 @@ struct CRuntimeClass
 	public:\
 	static const CRuntimeClass class##class_name;\
 	virtual CRuntimeClass* GetRuntimeClass() const;
-	
+
 #define IMPLEMENT_DYNAMIC(class_name,base_class) \
 	IMPLEMENT_RUNTIMECLASS(class_name,base_class,0xFFFF,NULL,NULL)
-	
+
 #define IMPLEMENT_RUNTIMECLASS(class_name,base_class,wSchema,pfnNew,class_init) \
 	const CRuntimeClass class_name::class##class_name = {\
 	#class_name,sizeof(class class_name),wSchema,pfnNew,RUNTIME_CLASS(base_class),NULL};\
 	CRuntimeClass* class_name::GetRuntimeClass() const{\
 		return RUNTIME_CLASS(class_name);};
-		
+
 #define RUNTIME_CLASS(class_name) _RUNTIME_CLASS(class_name)
-#define _RUNTIME_CLASS(class_name) ((CRuntimeClass*)(&class_name::class##class_name))	
+#define _RUNTIME_CLASS(class_name) ((CRuntimeClass*)(&class_name::class##class_name))
 
 class CObject
 {
 	DECLARE_DYNAMIC(CObject)
 public:
-	
+
 private:
-	
+
 };
 const CRuntimeClass CObject::classCObject = {
 	"CObject",sizeof(CObject),0xffff,NULL,NULL,NULL};
