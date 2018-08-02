f94bc8cd6935c9d26541fd05db6b45d8f376c5fa
yjyq1990 <695447272@qq.com>
2018-08-02 16:42:58 +0800

    add delete_0file&killp&get_ip&ps_ex&webserver function

--- tools/tool.sh
+++ tools/tool.sh
@@ -51,3 +51,73 @@ delete_space() {
 	sed 's/ //g' $1
 }
 
+#delete file which size is zero
+#usage:
+#	delete_0file DIR
+# 删除空文件
+#	find / -type f -size 0 -exec rm -rf {} \;
+delete_0file() {
+	if [ ! -d $1 ]; then
+		echo "error parameter:need a DIR path"
+		return 1
+	fi
+	find $1 -type f -size 0 -exec rm {} \;
+}
+
+#kill process with NAME
+#usage:
+#	killp NAME
+# 查找进程pid并kill
+#	pgrep nginx|xargs kill
+#	pidof nginx|xargs kill
+killp() {
+	if [ $# -lt 1 ]; then
+		echo "error:Need a process name"
+		return 1
+	fi
+	pgrep $1|xargs kill
+}
+
+#get ip address with NET_NAME device
+#usage:
+#	get_ip NET_NAME
+# 获取当前IP地址
+#	ifconfig |awk -F"[ ]+|[:]" 'NR==2 {print $4}'
+get_ip() {
+	if [ $# -lt 1 ]; then
+		echo "error:Need a net device name"
+		return 1
+	fi
+	ifconfig $1|awk -F"[ ]+|[:]" 'NR==2 {print $4}'
+}
+
+#sort process with memory usage
+#usage:
+#	ps_ex
+# 以内存大小排序列出进程
+#	ps aux --sort=rss |sort -k 6 -rn
+ps_ex() {
+	ps aux --sort=rss |sort -k 6 -rn
+}
+
+#start a web server on port 8000
+#usage:
+#	webserver
+# 简单web server列出当前目录文件，端口8000
+#	python -m SimpleHTTPServer
+webserver() {
+	python -m SimpleHTTPServer
+}
+
+mkcd() {
+	mkdir $1
+	cd $1
+}
+
+tmpstr() {
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1
+}
+
+wget_site() {
+	wget -r -p -np -k $1
+}
