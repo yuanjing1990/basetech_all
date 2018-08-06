58e2c4b100c89b19072d94bc1365c82841d9ecc8
yuanjing <yuanjing@iauto.com>
2018-08-05 19:40:22 -0700

    modify to run on Ubuntu 16.04

--- workdiary/utShell/init_env.sh
+++ workdiary/utShell/init_env.sh
@@ -4,12 +4,13 @@ install_software(){
 	sudo apt-get install git-core gnupg flex bison gperf build-essential \
 	zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
 	libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
-	libgl1-mesa-dev gcc-4.4 gcc-4.4-multilib mingw32 tofrodos \
+	libgl1-mesa-dev mingw-w64 tofrodos \
 	python-markdown libxml2-utils xsltproc zlib1g-dev:i386 u-boot-tools openssh-client libswitch-perl \
-	g++-4.4 g++-4.4-multilib cksfv uuid-dev:i386 liblzo2-dev:i386 lzop \
-	android-tools-adb lcov
+	cksfv uuid-dev:i386 liblzo2-dev:i386 lzop \
+	android-tools-adb lcov \
+	gcc gcc-multilib g++ g++-multilib
 }
-
+#gcc-4.4-multilib g++-4.4-multilib gcc-4.4 g++-4.4 
 #2.config git
 config_git(){
 	git config --global user.name $1
@@ -35,19 +36,22 @@ get_repo_tool(){
 
 #4.get project code
 get_project_code(){
-	if [ -d $1 ];then
-		rm -rf $1
+	if [ ! -d $1 ];then
+		mkdir $1
+		cd $1
+		repo init -u ssh://192.168.8.74:29418/Src/$1/platform/manifest -b $2
+		cd -
 	fi
 
-	mkdir $1
 	cd $1
-	repo init -u ssh://192.168.8.74:29418/Src/$1/platform/manifest -b $2
 	repo sync
-	repo start $2 -all
+	repo start $2 --all
+	cd -
 }
 
 #5.compile project
 compile_project(){
+	cd $1
 	source build/envsetup.sh
 	lunch full-eng
 	make -j4
@@ -70,5 +74,5 @@ if [ ! -e /usr/bin/repo ];then
 	get_repo_tool
 fi
 get_project_code ${PROJECT_NAME} ${BRANCH_NAME}
-compile_project
+compile_project ${PROJECT_NAME}
 
