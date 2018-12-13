9ea25af8ce21022c550b9c9dabc4d618389c6b29
yuanjing <yuanjing@iauto.com>
2018-12-13 18:19:58 +0800

    modify ThreadTest.java

--- java/Thread/ThreadTest.java
+++ java/Thread/ThreadTest.java
@@ -1,11 +1,71 @@
+import java.lang.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
 public class ThreadTest
 {
+	public AtomicInteger cntProduct = new AtomicInteger(0);
+	public static ThreadTest thdTest = new ThreadTest();
 	public static void main(String args[])
 	{
-		Thread t1 = new subThread("First");
-		Thread t2 = new subThread("Second");
-		t1.start();
-		t2.start();
+		thdTest.cntProduct.set(0);
+		ExecutorService pool = Executors.newFixedThreadPool(5);
+		pool.execute(thdTest.new Consumer(1));
+		pool.execute(thdTest.new Consumer(2));
+		pool.execute(thdTest.new Consumer(3));
+		pool.execute(thdTest.new Productor(1));
+		pool.execute(thdTest.new Productor(2));
+		pool.shutdown();
+	}
+	class Consumer implements Runnable {
+		int mId;
+		public Consumer(int id) {
+			mId = id;
+		}
+		public void run() {
+			while(true) {
+				try{
+					Thread.sleep(2000);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				synchronized(cntProduct) {
+					if(cntProduct.intValue() == 0) {
+						try {
+							System.out.println(String.format("Consume%d have no product,wait...",mId));
+							cntProduct.wait();
+						}catch(InterruptedException e) {
+							e.printStackTrace();
+						}
+						continue;
+					}
+					cntProduct.decrementAndGet();
+					System.out.println(String.format("Consume%d a product,left product number:%d",mId,cntProduct.intValue()));
+				}
+			}
+		}
+	}
+	class Productor implements Runnable {
+		int mId;
+		public Productor(int id) {
+			mId = id;
+		}
+		public void run() {
+			while(true) {
+				try{
+					Thread.sleep(1000);
+					if(cntProduct.intValue() >= 10)
+						Thread.sleep(20000);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				synchronized(cntProduct) {
+					cntProduct.incrementAndGet();
+					cntProduct.notifyAll();
+					System.out.println(String.format("Product%d a product,left product number:%d",mId,cntProduct.intValue()));
+				}
+			}
+		}
 	}
 }
 class subThread extends Thread
