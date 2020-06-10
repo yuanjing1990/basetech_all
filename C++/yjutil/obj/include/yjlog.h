e0579f3b8d2add97f12b4083fbf21b84d99b572a
yjyq1990 <695447272@qq.com>
2020-06-10 23:13:56 +0800

    modify yjutil

---
+++ C++/yjutil/obj/include/yjlog.h
@@ -0,0 +1,82 @@
+#ifndef YJLOG_H_
+#define YJLOG_H_
+
+#include <iostream>
+
+#include "yjdef.h"
+
+namespace yjutil {
+
+// ==========log util class===========->Begin
+/**
+ * @addtogroup print_tools
+ * @{
+ */
+
+/**
+ * @brief Calculate time period for program.
+ * Calculate time period for program,and print start and stop
+ * info
+ *
+ */
+struct _Tick {
+	/**
+	 * @brief Start the clock.
+	 *
+	 * @return The start clock in milisecond.
+	 *
+	 */
+	static clock_t start();
+
+	/**
+	 * @brief Start the clock and print info.
+	 *
+	 * @return The start clock in minisecond.
+	 *
+	 */
+	static clock_t start_out();
+
+	/**
+	 * @brief Stop the clock and print result.
+	 *
+	 * @return The end clock in minisecond.
+	 *
+	 */
+	static clock_t end();
+
+	/**
+	 * @brief Print the clock and print result.
+	 * Print the clock and print result,and then print clock will stop.
+	 *
+	 * @return zero,which is useless
+	 *
+	 */
+	static clock_t end_out();
+
+  private:
+	static clock_t m_start;
+	static bool m_bflag;
+};
+
+/**
+ * @brief print stl vector type collection.
+ * print stl collection, which have begin() and end() member
+ *
+ * @param vec,the collection will be print.
+ *
+ */
+template <class T> void print(const T &vec) {
+	using namespace std;
+	for (typename T::const_iterator _it = vec.begin(); _it != vec.end(); ++_it)
+		cout << *_it << ' ';
+	cout << endl;
+}
+
+/**
+ * @}
+ */
+//==========log util class===========-<End
+
+}
+
+#endif
