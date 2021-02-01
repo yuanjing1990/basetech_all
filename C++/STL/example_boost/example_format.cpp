a657f52b38000ccaa328721baa2effb951397daa
yuanjing1113 <yuanjing1113@thundersoft.com>
2021-02-01 20:26:36 +0800

    modify to cmakefile

--- C++/STL/example_boost/example_format.cpp
+++ C++/STL/example_boost/example_format.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
     std::cout << boost::format("writing %1%,  x=%2% : %3%-th try") % "toto" %
                      40.23 % 50
               << std::endl;
-    boost::format ft("%d%, %d%");
+    boost::format ft("%1%, %2%");
     ft % "hello" % "world";
     std::cout << ft.str() << std::endl;
     std::cout << boost::str(ft) << std::endl;
